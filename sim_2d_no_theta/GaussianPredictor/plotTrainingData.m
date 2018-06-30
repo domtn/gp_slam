85dd56c00e30dd37331d96e816c692c8c81a8b9d
Tung Nguyen <tung.d.nguyen@gmail.com>
2018-06-30 09:26:02 +0800

    Update plotTrainingData.m

--- sim_2d_no_theta/GaussianPredictor/plotTrainingData.m
+++ sim_2d_no_theta/GaussianPredictor/plotTrainingData.m
@@ -10,8 +10,8 @@ occGridFile = [occGridFilePath, occGridFileName];
 
 id = fopen(occGridFile);
 for i=1:8
-	readin = fgetl(id);
-	para(i) = str2num(readin(16:length(readin)));        
+    readin = fgetl(id);
+    para(i) = str2num(readin(16:length(readin)));        
 end
 
 xMin = para(1); % Bottom Left corner of the map
@@ -37,9 +37,9 @@ trainingDataFile = [trainingDataPath, trainingDataFileName];
 trainingData = load(trainingDataFile);
 
 TrainingInputs = trainingData(2:size(trainingData,1), ...
-	             2:4);
+                              2:4);
 TrainingObservations = trainingData(2:size(trainingData,1), ...
-								   5:size(trainingData,2));
+                                    5:size(trainingData,2));
 observationAngles = trainingData(1, 5:size(trainingData,2))';
 
 observationAngles = observationAngles.*pi/180.0;
@@ -48,7 +48,7 @@ xBeams = [];
 yBeams = [];
 x_impact = zeros(1,size(observationAngles,1));
 y_impact = zeros(1,size(observationAngles,1));
-				
+
 i = 1;
 figure;
 hold on; 
@@ -58,54 +58,48 @@ axis([xMin-20 xMax+20 yMin-20 yMax+20]);
 imagesc([xMin xMax], [yMin yMax], occGrid);
 colormap(gray);
 
-    	
-	
+
 for training_id = 1:size(TrainingInputs,1)
 
-	disp([training_id, '=', num2str(training_id)]);
+    disp([training_id, '=', num2str(training_id)]);
 
-	xPose = TrainingInputs(training_id,1);
-	yPose = TrainingInputs(training_id,2);
-	thetaPose = TrainingInputs(training_id,3);
+    xPose = TrainingInputs(training_id,1);
+    yPose = TrainingInputs(training_id,2);
+    thetaPose = TrainingInputs(training_id,3);
 
-	observation = TrainingObservations(training_id, :)';
+    observation = TrainingObservations(training_id, :)';
 
-	xBeams = [];
-	yBeams = [];
+    xBeams = [];
+    yBeams = [];
 
-	for beam_id=1:size(observationAngles,1)
-		[x_impact(1,beam_id), y_impact(1,beam_id)] = FrameSensorToInertial(xPose, ...
-																		   yPose, ...
-																		   thetaPose, ...
-																		   observationAngles(beam_id,1),... 
-																		   observation(beam_id,1), 1);
-		xBeams = [xBeams, xPose, x_impact(1,beam_id)];
-		yBeams = [yBeams, yPose, y_impact(1,beam_id)];
-	end
-	size(observation)
+    for beam_id=1:size(observationAngles,1)
+        [x_impact(1,beam_id), y_impact(1,beam_id)] = FrameSensorToInertial(xPose, ...
+                                                                            yPose, ...
+                                                                            thetaPose, ...
+                                                                            observationAngles(beam_id,1),... 
+                                                                            observation(beam_id,1), 1);
+        xBeams = [xBeams, xPose, x_impact(1,beam_id)];
+        yBeams = [yBeams, yPose, y_impact(1,beam_id)];
+    end
+    size(observation)
 
-	if (training_id == 1) 
-		hImpact = plot(x_impact, y_impact, '.r');
-		hBeams = plot(xBeams, yBeams, '-r');
-	else
-		set(hImpact, 'XData', x_impact);
-		set(hImpact, 'YData', y_impact);
-		set(hBeams, 'XData', xBeams);
-		set(hBeams, 'YData', yBeams);
-	end
+    if (training_id == 1) 
+        hImpact = plot(x_impact, y_impact, '.r');
+        hBeams = plot(xBeams, yBeams, '-r');
+    else
+        set(hImpact, 'XData', x_impact);
+        set(hImpact, 'YData', y_impact);
+        set(hBeams, 'XData', xBeams);
+        set(hBeams, 'YData', yBeams);
+    end
 
-	[x_heading, y_heading] = FrameSensorToInertial(xPose, ...
-												   yPose, ...
-												   thetaPose, 0.0, 2.0, 1);        
-	plot([xPose, x_heading], [yPose, y_heading], 'y-');        
-	plot(xPose, yPose, 'yo');
+    [x_heading, y_heading] = FrameSensorToInertial(xPose, ...
+                                                    yPose, ...
+                                                    thetaPose, 0.0, 2.0, 1);        
+    plot([xPose, x_heading], [yPose, y_heading], 'y-');        
+    plot(xPose, yPose, 'yo');
 
-% 	pause;
-
-
-end
-    
-     
+%   pause;
 
 end
 
@@ -119,6 +113,7 @@ x_SensorFrame = x_translated*cos(thetaS) + y_translated*sin(thetaS);
 y_SensorFrame = y_translated*cos(thetaS) - x_translated*sin(thetaS);
 
 end
+
 
 function [x_InertialFrame, y_InertialFrame] = FrameSensorToInertial(xS, yS, thetaS, param1, param2, NotCart)
     if (NotCart == 1)
@@ -135,5 +130,4 @@ function [x_InertialFrame, y_InertialFrame] = FrameSensorToInertial(xS, yS, thet
     x_InertialFrame = x_rotated + xS;
     y_InertialFrame = y_rotated + yS;   
 end
-
 
